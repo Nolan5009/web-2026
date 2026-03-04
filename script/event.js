bc51c4f0a434c8e9cf78efa116c990bb9e27fb86
Nolan5009 <ntnakamura.1@gmail.com>
2026-03-04 17:31:55 +0000

    Wed, Mar 4, 2026, 9:31 AM -08:00

---
+++ script/event.js
@@ -0,0 +1,46 @@
+clickBox = document.querySelector('.click')
+clickToSpin = document.querySelector('.spin')
+moveBox = document.querySelector('.move')
+hoverBox = document.querySelector('.hover')
+dblClickBox = document.querySelector('.dblClick')
+
+console.log(clickBox)
+console.log(moveBox)
+
+colors = ['red', 'green', 'yellow', 'purple', 'violet']
+index = 0
+
+clickToSpin.addEventListener('click', () => {
+    clickToSpin.classList.toggle('spinning')
+})
+
+
+clickBox.addEventListener('click', () => {
+    clickBox.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length) {
+        index = 0
+    }
+    clickBox.style.color = 'white'
+})
+
+x = 0
+y = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {x = x + 10}
+    if (event.key == 'ArrowUp') {y = y - 10}
+    if (event.key == 'ArrowLeft') {x = x - 10}
+    if (event.key == 'ArrowDown') {y = y + 10}
+    moveBox.style.transform =  `translate(${x}px, ${y}px)`
+})
+
+hoverBox.addEventListener('mouseenter', () => {
+    hoverBox.style.height = '100px'
+    hoverBox.style.width = '100px'
+})
+
+hoverBox.addEventListener('mouseLeave', () => {
+    hoverBox.style.height = '80px'
+    hoverbox.style.width = '80px'
+})

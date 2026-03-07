0887e7e80e2eabbcedef13b604fe13f3c66a7dd5
Nolan5009 <ntnakamura.1@gmail.com>
2026-03-07 20:25:30 +0000

    Sat, Mar 7, 2026, 12:25 PM -08:00

--- script/interactive.js
+++ script/interactive.js
@@ -1,8 +1,8 @@
 clickbtn = document.querySelector('.button')
 bg = document.querySelector('.bg')
-movebtn = document.querySelector('btn')
+movebtn = document.querySelector('.btn')
 
 clickbtn.addEventListener('click', () => {
     bg.classList.toggle('active')
-    movebtn.style.transform = "translate(50px, 0px)"
+    movebtn.classList.toggle('active')
 })

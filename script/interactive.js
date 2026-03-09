50d3abd7d8f3d21344f33c7e436cc34986b151ff
Nolan5009 <ntnakamura.1@gmail.com>
2026-03-09 16:54:57 +0000

    Mon, Mar 9, 2026, 9:54 AM -07:00

--- script/interactive.js
+++ script/interactive.js
@@ -2,9 +2,11 @@ clickbtn = document.querySelector('.button')
 bg = document.querySelector('.bg')
 btn1 = document.querySelector('.btn')
 btn2 = document.querySelector('.btn2')
+text = document.querySelector('.text')
 
 clickbtn.addEventListener('click', () => {
     bg.classList.toggle('active')
     btn1.classList.toggle('active')
     btn2.classList.toggle('active')
+    text.classList.toggle('active')
 })

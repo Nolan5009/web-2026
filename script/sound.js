3e251247fad5b158672338a20e77ab6c39195cb7
Nolan5009 <ntnakamura.1@gmail.com>
2026-03-11 16:44:10 +0000

    Wed, Mar 11, 2026, 9:44 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,13 +1,20 @@
 buttons = document.querySelector(".buttons")
 applause = document.querySelector('.applause')
-boo = document.querySelecor('.boo')
+boo = document.querySelector('.boo')
+gasp = document.querySelector('.gasp')
+tada = document.querySelector('.tada')
+victory = document.querySelector('.victory')
+wrong = document.querySelector('.wrong')
 
-btn = document.createElement('button')
-btn.innerText = "applause"
-btn.classList.add('btn')
-btn.addEventListener('click', () =>{
-    applause.play()
-})
-buttons.appendChild(btn)
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
+for (let i = 0; i > 6; i = i + 1){
+    btn = document.createElement('button')
+    btn.innerText = "sounds[i]"
+    btn.classList.add('btn')
+    btn.addEventListener('click', () =>{
+        document.querySelector(`.$sounds[i]`).play()
+    })
+    buttons.appendChild(btn)
+}
 

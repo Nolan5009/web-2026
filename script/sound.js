0f72cbbf8c1a3d43a48041e1c2604a59e299443a
Nolan5009 <ntnakamura.1@gmail.com>
2026-03-11 16:56:30 +0000

    Wed, Mar 11, 2026, 9:56 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -2,13 +2,21 @@ buttons = document.querySelector('.buttons')
 
 sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
-for (let i = 0; i > 6; i = i + 1){
+for (let i = 0; i < 6; i = i + 1){
     btn = document.createElement('button')
     btn.innerText = sounds[i]
     btn.classList.add('btn')
     btn.addEventListener('click', () =>{
+        stopSounds()
         document.querySelector(`.$sounds[i]`).play()
     })
     buttons.appendChild(btn)
+}
+
+function stopSounds(){
+    for (let i = 0; i < sounds.length; i = i + 1) {
+        document.querySelector(`.$sounds[i]`).pause()
+        document.querySelector(`.$sounds[i]`).currentTime = 0
+    }
 }
 

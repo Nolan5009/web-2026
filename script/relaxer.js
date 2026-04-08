b79788a9f9a7d7a1e31ae06592533e39d8a31bb1
Nolan5009 <ntnakamura.1@gmail.com>
2026-04-08 16:10:55 +0000

    Wed, Apr 8, 2026, 9:10 AM -07:00

--- script/relaxer.js
+++ script/relaxer.js
@@ -1,1 +1,6 @@
-container = document.querySelector('.container')
+container = document.getElementById('container')
+text = document.getElementById('text')
+
+totalTime = 7500
+breathTime = (totalTime / 5) * 2
+holdTime - (totalTIme /5)

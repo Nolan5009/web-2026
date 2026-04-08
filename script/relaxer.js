19917df0148098c3cabd8343949c1c3e49dc6c99
Nolan5009 <ntnakamura.1@gmail.com>
2026-04-08 16:14:37 +0000

    Wed, Apr 8, 2026, 9:14 AM -07:00

--- script/relaxer.js
+++ script/relaxer.js
@@ -4,3 +4,9 @@ text = document.getElementById('text')
 totalTime = 7500
 breathTime = (totalTime / 5) * 2
 holdTime - (totalTIme /5)
+
+breathAnimation()
+
+function breathAnimation() {
+    text.innerText = 'Breath In'
+}
